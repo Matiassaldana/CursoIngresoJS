ee34670a7d728230d5dd5c64f0b23768ef3d3250
Matias <matiasjs139@gmail.com>
2022-01-31 19:08:41 -0300

    Enunciado correcto

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,11 @@
 /*
+Saldaña Matias
+e/s 01
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
+	//Alert ("Hola mundo"); esto no funciona
 	alert("Esto funciona de maravilla");
 }
 
